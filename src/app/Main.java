69ca0dcaa860b9b938ea883ec2923051b126867c
Ilya <ilyakolesnik2048@gmail.com>
2026-04-09 18:46:41 +0300

    Conversion function of miles to km added

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter Project: miles - kilometers");
     }
+
+    public static double milesToKm(double miles) {
+        return miles * 1.60934;
+    }
 }
