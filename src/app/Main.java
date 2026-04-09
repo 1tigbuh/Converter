45ba0b85c67faed18bb7f5b427ed25a50cdd73cc
Ilya <ilyakolesnik2048@gmail.com>
2026-04-09 18:48:06 +0300

    Conversion function of km to to miles added

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,4 +8,8 @@ public class Main {
     public static double milesToKm(double miles) {
         return miles * 1.60934;
     }
+
+    public static double kmToMiles(double km) {
+        return km / 1.60934;
+    }
 }
